40c66408b7792fd5077a7afa1f9ac1b409540c7e
ankan kumar <ankankumar071@gmail.com>
2023-07-16 01:28:52 +0530

    monkeytask

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const Header =()=>{
+    return(
+        <>
+        <nav >Get ready to complete your daily Goals</nav>
+     </>
+    )
+
+}
+
+
+
+export default Header;
